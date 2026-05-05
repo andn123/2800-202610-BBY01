732cb85763da143fbaf79a755e9d737dfd0f6642
yuzuruus <72951653+yuzuruus@users.noreply.github.com>
2026-05-05 13:07:19 -0700

    Set up the basic signup login page

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const MongoStore = require("connect-mongo").default;
 require("dotenv").config();
 
 const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello world");
+});
+
+app.get("/login", (req, res) => {
+  res.render("SignUpLogIn", {
+    title: "Login",
+    CSSFiles: ["SignUpLogIn.css"],
+    JSFiles: ["SignUpLogIn.js"],
+  });
 });
 
 app.listen(3000, () => {
