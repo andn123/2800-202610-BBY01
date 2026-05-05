5a0f054c75a31365b1fa70e0c10a8f4dcdc58ea5
andn123 <andrewni0123@gmail.com>
2026-05-04 18:57:12 -0700

    Move index.js to root directory

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+const MongoStore = require("connect-mongo").default;
+require("dotenv").config();
+
 const app = express();
 
 app.get("/", (req, res) => {
